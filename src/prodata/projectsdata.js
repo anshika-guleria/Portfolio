95a078f7e3438c066138b94de940950e6bb14b2a
Anshika Guleria <anshikaguleria532@gmail.com>
2026-04-05 11:26:57 +0530

    Projects added

--- src/prodata/projectsdata.js
+++ src/prodata/projectsdata.js
@@ -1,8 +1,45 @@
 // src/data/projectsData.js
 import LifeShare from "../assets/LifeShare.png";
+import PhotoBooth from "../assets/PhotoBooth.png";
+import PrepPal from "../assets/PrepPal.png";
+import ShelfReview from "../assets/ShelfReview.png";
 import ThinkBoard from "../assets/ThinkBoard.png";
 import WordWaltz from "../assets/WordWaltz.png";
 export const projects = [
+   {
+    title: "PrepPal",
+    image: PrepPal,
+      mobileImageHeight: "96px",
+    description:
+      "",
+    technologies: ["React.js", "Node.js", "MongoDB", "Express","Tailwind","Daisyui","Socket.io","webRTC","PeerJS"],
+    links: {
+      github: "https://github.com/anshika-guleria/PrepPal",
+    },
+  },
+   {
+    title: "PhotoBooth",
+    image: PhotoBooth,
+      mobileImageHeight: "96px",
+    description:
+      "A fun, themed browser photo booth built with React. Take a 3-photo strip with your webcam, add stickers, apply filters, pick a glitter border, and download the finished strip as a PNG.",
+    technologies: ["React", "JavaScript", "CSS"],
+    links: {
+      github: "https://github.com/anshika-guleria/PhotoBooth",
+      vercel: "https://photo-booth-sigma-nine.vercel.app/"
+    },
+  },
+   {
+    title: "Shelf Review",
+    image: ShelfReview,
+      mobileImageHeight: "96px",
+    description:
+      "Shelf Review is a mobile app for book enthusiasts to share and explore reviews. It provides a simple, interactive, and mobile-friendly interface for discovering books and connecting with readers.",
+    technologies: ["React Native", "Node.js", "MongoDB", "Express","CSS","JWT"],
+    links: {
+      github: "https://github.com/anshika-guleria/Shelf_Review_Frontend",
+    },
+  },
   {
     title: "ThinkBoard",
     image: ThinkBoard,
